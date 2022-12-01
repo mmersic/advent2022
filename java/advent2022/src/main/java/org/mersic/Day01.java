f13d1868bfeb7416aa8cba67235ac7d5247df99a
mmersic <mmersic@gmail.com>
2022-12-01 11:12:16 -0500

    Day 1

---
+++ java/advent2022/src/main/java/org/mersic/Day01.java
@@ -0,0 +1,23 @@
+package org.mersic;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Day01 {
+    public static void main(String[] args) throws Exception {
+        /* To be clear, this is just for fun. I would never write code like this for a real application! */
+        List<Integer> cals = Arrays.stream(Files.readAllLines(Path.of(Day01.class.getClassLoader().getResource("day.01.input").toURI()))
+                        .stream().map(x -> x.equals("") ? "X" : x).reduce("", (a, b) -> a + "," + b).split("X"))
+                .map(y -> y.split(","))
+                .map(z -> Arrays.stream(z).filter(a -> !a.equals(""))
+                        .map(Integer::parseInt).reduce(0, Integer::sum))
+                .sorted(Comparator.reverseOrder())
+                .toList();
+
+        System.out.println("part1: " + cals.get(0));
+        System.out.println("part2: " + cals.stream().limit(3).reduce(0, Integer::sum));
+    }
+}
