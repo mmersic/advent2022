21b47474f8e569b0a446b65fa9ae902bf59c70fc
mmersic <mmersic@gmail.com>
2022-12-02 22:46:55 -0500

    Rust impl of day 1 and 2.

---
+++ rust/advent2022/src/day_01.rs
@@ -0,0 +1,12 @@
+pub fn go() {
+    let mut cals: _ = include_str!("../inputs/day.01.input")
+        .split("\n\n")
+        .map(|x| x.split("\n").map(|x | x.parse::<i32>().unwrap()).sum())
+        .collect::<Vec<i32>>();
+    
+    cals.sort_by(|x, y| x.cmp(y).reverse());
+    
+    println!("day 1 part 1: {}", cals[0]);
+    println!("day 1 part 2: {}", cals[0] + cals[1] + cals[2]);
+    
+}
---
+++ rust/advent2022/src/main.rs
@@ -0,0 +1,11 @@
+mod day_01;
+mod day_02;
+
+fn all() {
+    day_01::go();
+    day_02::go();    
+}
+
+fn main() {
+    all();
+}
