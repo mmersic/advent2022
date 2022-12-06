60b40601020a342063d391c690c303f4501bdc89
mmersic <mmersic@gmail.com>
2022-12-06 15:53:27 -0500

    Day 6 java, streamlined a bit

--- java/advent2022/src/main/java/org/mersic/Day06.java
+++ java/advent2022/src/main/java/org/mersic/Day06.java
@@ -9,22 +9,21 @@ public class Day06 {
     public static int scanForDistinct(char[] chars, int distinctCount) {
         int[] seen = new int[256];
         int distinct = 0;
-        int len = 0;
-        for (int i = 0; i < chars.length; i++) {
+        
+        for (int i = 0; i < distinctCount; i++) {
+            if (++seen[chars[i]] == 1) {
+                distinct++;
+            }
+        }
+        
+        for (int i = distinctCount; i < chars.length; i++) {
             if (distinct == distinctCount) {
                 return i;
             }
-            if (len == distinctCount) {
-                char c = chars[i-distinctCount];
-                seen[c]--;
-                len--;
-                if (seen[c] == 0) {
-                    distinct--;
-                }
+            if (--seen[chars[i-distinctCount]] == 0) {
+                distinct--;
             }
-            seen[chars[i]]++;
-            len++;
-            if (seen[chars[i]] == 1) {
+            if (++seen[chars[i]] == 1) {
                 distinct++;
             }
         }
@@ -34,9 +33,8 @@ public class Day06 {
     public static void main(String args[]) throws Exception {
         List<String> input = Files.readAllLines(Path.of(Day05.class.getClassLoader().getResource("day.06.input").toURI()));
         char[] chars = input.get(0).toCharArray();
-        
+
         System.out.println("day 6 part 1: "+ scanForDistinct(chars, 4));
         System.out.println("day 6 part 2: "+ scanForDistinct(chars, 14));
-        
     }
 }
