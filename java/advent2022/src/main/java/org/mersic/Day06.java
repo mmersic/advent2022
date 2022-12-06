6d28bd303121756e19b0b676095ec04ea5561502
mmersic <mmersic@gmail.com>
2022-12-06 15:22:26 -0500

    Day 6 java, no sets, O(n)

--- java/advent2022/src/main/java/org/mersic/Day06.java
+++ java/advent2022/src/main/java/org/mersic/Day06.java
@@ -2,36 +2,41 @@ package org.mersic;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Day06 {
+    
+    public static int scanForDistinct(char[] chars, int distinctCount) {
+        int[] seen = new int[256];
+        int distinct = 0;
+        int len = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (distinct == distinctCount) {
+                return i;
+            }
+            if (len == distinctCount) {
+                char c = chars[i-distinctCount];
+                seen[c]--;
+                len--;
+                if (seen[c] == 0) {
+                    distinct--;
+                }
+            }
+            seen[chars[i]]++;
+            len++;
+            if (seen[chars[i]] == 1) {
+                distinct++;
+            }
+        }
+        throw new RuntimeException((distinctCount + " chars not found"));
+    }
+    
     public static void main(String args[]) throws Exception {
         List<String> input = Files.readAllLines(Path.of(Day05.class.getClassLoader().getResource("day.06.input").toURI()));
         char[] chars = input.get(0).toCharArray();
-
-        for (int i = 3; i < chars.length; i++) {
-            Set<Character> set = new HashSet<>();
-            for (int j = 0; j < 4; j++) {
-                set.add(chars[i-j]);
-            }
-            if (set.size() == 4) {
-                System.out.println("day 6 part 1: "+ (i+ 1));
-                break;
-            }
-        }
-
-        for (int i = 13; i < chars.length; i++) {
-            Set<Character> set = new HashSet<>();
-            for (int j = 0; j < 14; j++) {
-                set.add(chars[i-j]);
-            }
-            if (set.size() == 14) {
-                System.out.println("day 6 part 2: "+ (i+ 1));
-                break;
-            }
-        }
+        
+        System.out.println("day 6 part 1: "+ scanForDistinct(chars, 4));
+        System.out.println("day 6 part 2: "+ scanForDistinct(chars, 14));
         
     }
 }
